02193d60b6d5c08bb9c077770b14d23ae4e536e8
s greenbaum <gbs4ever2@gmail.com>
2018-12-19 02:57:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,12 @@ require 'pry'
 
 def reverse_each_word(string)
 array=string.split()
+binding.pry
 answer =[]
 array.each do |p|
 answer <<  p.reverse
    end
 
-binding.pry
-#array.each do |x|
 
-  #  x.reverse
+
 end
